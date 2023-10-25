17e74f7466bb4ecfcd4eb3881c8d6b512411c2f6
Tornike Sikharulidze <92145572+sixxa@users.noreply.github.com>
2023-10-25 23:04:04 +0400

    Add files via upload

---
+++ c++projects/PerfNum.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+   for(int i=1; i<1000000; i++){
+        int sum=0;
+        for(int j=1; j<i; j++){
+            if(i%j==0){
+                sum+=j;
+            }
+        }
+        if(sum==i){
+           cout<<sum<<endl;
+        }
+    }
+    return 0;
+
+}
